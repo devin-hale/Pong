722ed0e60efb0d52b2f62162a5a2d39464cd47fe
devin-hale <devinwaynehale@gmail.com>
2023-12-30 20:41:55 -0600

    Created PlayerScore class and linked cmake with SDL2_ttf

---
+++ src/PongLib/PlayerScore/PlayerScore.h
@@ -0,0 +1,13 @@
+#ifndef PLAYERSCORE_H
+#define PLAYERSCORE_H
+
+#include <SDL_ttf.h>
+
+#include "../../Engine/Renderer/Renderer.h"
+
+class PlayerScore {
+   private:
+    Renderer* m_renderer{nullptr};
+};
+
+#endif
